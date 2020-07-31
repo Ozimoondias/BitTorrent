aaec9859835f6dc3e87df848cf70a255aa5d6b85
theo <theo.goetschy@gmail.com>
2020-08-01 01:29:57 +0200

    total refactor

---
+++ cli/src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by agar on 01/08/2020.
+//
+
+#include    <iostream>
+
+int         main()
+{
+    while (1)
+        std::cout << "test cli" << std::endl;
+    return 0;
+}
---
+++ cmake-build-debug/daemon/CMakeFiles/BT_daemon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BT_daemon"
+  "BT_daemon.pdb"
+  "CMakeFiles/BT_daemon.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BT_daemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
